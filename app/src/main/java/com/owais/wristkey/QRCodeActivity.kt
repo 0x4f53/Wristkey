a8319484e56e8fd1f9961308b13ec89323f70464
Owais Shaikh <0x4f@tuta.io>
2021-05-16 19:52:48 +0530

    added QR Code scanner

---
+++ app/src/main/java/com/owais/wristkey/QRCodeActivity.kt
@@ -0,0 +1,11 @@
+package com.owais.wristkey
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QRCodeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qrcode)
+    }
+}
