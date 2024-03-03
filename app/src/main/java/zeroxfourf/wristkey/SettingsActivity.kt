ac9cb5b66dcc5b8f97087cdc9337b88357e9a35d
0x4f53 <71916237+0x4f53@users.noreply.github.com>
2024-03-03 23:10:13 +0530

    fixed compact mode bug

--- app/src/main/java/zeroxfourf/wristkey/SettingsActivity.kt
+++ app/src/main/java/zeroxfourf/wristkey/SettingsActivity.kt
@@ -3,6 +3,7 @@ package zeroxfourf.wristkey
 import android.app.KeyguardManager
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
